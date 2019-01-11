41b455997880b21367ae684fb47f5a40d91af8da
Tyson <381980920@qq.com>
2019-01-12 02:23:37 +0800

    new

---
+++ 移除元素.py
@@ -0,0 +1,28 @@
+'''
+示例 1:
+给定 nums = [3,2,2,3], val = 3,
+函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+你不需要考虑数组中超出新长度后面的元素。
+
+示例 2:
+给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+注意这五个元素可为任意顺序。
+你不需要考虑数组中超出新长度后面的元素。
+'''
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        length = len(nums)
+        for i in range(length):
+            while i < len(nums) and nums[i] == val:
+                del nums[i]
+        return len(nums)
+
+nums = [0,1,2,2,3,0,4,2]; val = 2
+t = Solution()
+print(t.removeElement(nums,val))
