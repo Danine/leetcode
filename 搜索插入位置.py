82a2a8b8c8259c4fe6a80b798abf57d756ac5538
Tyson <381980920@qq.com>
2019-01-12 18:24:45 +0800

    做完最快的一次

---
+++ 搜索插入位置.py
@@ -0,0 +1,23 @@
+'''
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+你可以假设数组中无重复元素。
+示例 1:                 示例 2:                 示例 3:
+输入: [1,3,5,6], 5      输入: [1,3,5,6], 2      输入: [1,3,5,6], 7
+输出: 2                 输出: 1                 输出: 4
+'''
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # 遍历nums，如果target小于等于nums，就插在这个地方
+        for i in range(len(nums)):
+            if target <= nums[i]:
+                return i
+        return len(nums)
+
+nums = [1,3,5,6]; target = 7
+t = Solution()
+print(t.searchInsert(nums,target))
