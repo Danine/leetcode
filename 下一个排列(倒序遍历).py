b2b9cbc7ac79a75f7e9321d5c0ac63a3062f3d9b
Tyson <381980920@qq.com>
2019-02-13 11:33:27 +0800

    完成！

--- 下一个排列(倒序遍历).py
+++ 下一个排列(倒序遍历).py
@@ -7,8 +7,8 @@
 3,2,1 → 1,2,3
 1,1,5 → 1,5,1
 1234 → 1243 → 1324 → 1342 → 1423 → 1432
-从数组倒着查找，找到nums[i] 比nums[i+1]小的时候，就将nums[i]跟nums[i+1]到nums[nums.length - 1]当中
-找到一个最小的比nums[i]大的元素交换。交换后，再把nums[i+1]到nums[nums.length-1]排序
+从数组倒着查找，找到nums[i-1] 比nums[i]小的时候，就将nums[i-1]跟nums[i]到nums[nums.length - 1]当中
+找到一个最小的比nums[i-1]大的元素交换。交换后，再把nums[i]到nums[nums.length-1]排序
 '''
 class Solution:
     def nextPermutation(self, nums):
@@ -17,14 +17,16 @@ class Solution:
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         length = len(nums)
-        for i in range(length-1,-1,-1):
-            if nums[i]>nums[i-1]:
-                more = []
-                for j in nums[i:length]:
-                    if j > nums[i-1]:
-                        more.append(j)
-                temp = min(more)
-                nums[nums.index(min(more))] = nums[i-1]
+        for i in range(length-1,-1,-1): #倒序遍历
+            if nums[i] > nums[i-1]:
+                mark = i
+                more = nums[i]
+                for j in range(i,length):   #查找最小的比nums[i-1]大的元素
+                    if nums[j] > nums[i-1] and nums[j] < more:
+                        more = nums[j]
+                        mark = j
+                temp = more
+                nums[mark] = nums[i-1]
                 nums[i-1] = temp
                 sort = nums[i:length]
                 sort.sort()
@@ -33,6 +35,6 @@ class Solution:
                 nums.extend(sort)
                 return nums
 
-nums = [5,4,7,5,3,2]
+nums = [1,2,3,4]
 t = Solution()
 print(t.nextPermutation(nums))
